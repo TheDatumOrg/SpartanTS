e7016852d93f23b91829da3e2603e69dfd56318f
Ivan-Fan <ivanyangfan09@gmail.com>
2025-08-25 16:51:19 +0000

    update configs for sax variants

--- benchmark/eval_classfication.py
+++ benchmark/eval_classfication.py
@@ -124,10 +124,6 @@ if __name__ == "__main__":
         clf = SAXDRDictionaryClassifier(**model_kwargs)
     elif classifier_name =='sax_vfd':
         clf = SAXVFDDictionaryClassifier(**model_kwargs)
-    elif classifier_name =='asax':
-        clf = ASAXDictionaryClassifier(**model_kwargs)
-    elif classifier_name =='twa_sax':
-        clf = TWASAXDictionaryClassifier(**model_kwargs)
     elif classifier_name == 'sfa':
         if config is None:
             clf = SFADictionaryClassifier(save_words=True)
